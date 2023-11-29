654fc7f2cc126ed63bbcbfdddea6f5634e243c48
CoderAsh1 <ashutoshsasmal1@gmail.com>
2023-11-29 22:20:44 +0530

    theme context added

---
+++ context/ThemeProvider.tsx
@@ -0,0 +1,42 @@
+// "use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+interface ThemeContextType {
+  mode: string;
+  setMode: (mode: string) => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export default function ThemeProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [mode, setMode] = useState("light");
+  const handleChangeTheme = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.add("light");
+    }
+  };
+
+  useEffect(() => {
+    handleChangeTheme();
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  return context;
+}
